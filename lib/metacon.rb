670bd23928317da1ad13d296a14ea458a99bbc21
Joseph Wecker <jwecker@justin.tv>
2011-09-16 17:06:08 -0700

    Expose version

---
+++ lib/metacon.rb
@@ -0,0 +1,8 @@
+module MetaCon
+  VERSION = File.exist?(File.join(File.dirname(__FILE__),'VERSION')) ?
+    File.read(File.join(File.dirname(__FILE__),'VERSION')) : ""
+
+  class << self
+    def version() VERSION end
+  end
+end
