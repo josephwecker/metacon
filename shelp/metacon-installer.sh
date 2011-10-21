1420cfc378b466e19396275067c9253f9d0e4cb7
Joseph Wecker <joseph.wecker@gmail.com>
2011-10-20 19:49:24 -0700

    Quick explanation and some minor tweaks so that it doesn't die prematurely

--- shelp/metacon-installer.sh
+++ shelp/metacon-installer.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-# TODO:
-#  - install the metacon gem (in metacon's ruby+gemset)
-#  - look at output to installation - find full path for .metacon_unwrapped
-#  - make a 'metacon' bin or symlink one that wraps .metacon_unwrapped w/ rvm exec
+cat <<EOF
+metacon-installer
+-----------------
+This will perform the following actions, all of which can be pretty easily
+undone if needed:
 
+  * Try to install rvm if it's not already installed
+  * Update rvm
+  * Install ruby 1.9.2 (or 1.9.3) safely isolated (by rvm)
+    from your system ruby
+  * Create the rvm gemset that metacon will run under and
+    install some gems into it
+  * Install into it the actual metacon gem
+  * Attempt to make a symlink for the gem's metacon binary
+    in /usr/local/bin to override rubygem's normal
+    executable handling and to make sure it's available no
+    matter what rvm gemset you happen to be in.
+
+EOF
+
+read -p "Continue (y/n)? "
+[ "$REPLY" == "y" ] || exit 1
 
 set -e
 
@@ -19,6 +36,7 @@ fi
 [[ `git --version` ]] || ( echo "Metacon requires 'git' (git-core package if deb/ubu). Please install and retry." && exit 1 )
 [[ `curl --version` ]] || ( echo "Metaon requires 'curl'. Please install and retry." && exit 1 )
 
+set +e
 if [[ ! `rvm version` ]]; then
   echo "RVM not found. Attempting to install."
   bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer ) || exit 2
@@ -32,7 +50,6 @@ else
 fi
 
 source "$HOME/.rvm/scripts/rvm"
-set +e
 [[ `rvm use $MCON_RUBY_V` == *Using* ]] || ( rvm install $MCON_RUBY_V && rvm use $MCON_RUBY_V ) || exit 3
 rvm use $MCON_RUBY_V
 [[ `rvm use $MCON_RUBY_V; rvm gemset use metacon 2>&1` == *ERROR* ]] && ( ( rvm use $MCON_RUBY_V && rvm gemset create metacon && rvm gemset use metacon ) || exit 3 )
@@ -48,6 +65,7 @@ unlink $GEMOUT
 TMPGEMDIR=`rvm gemdir`
 
 set +e
+echo "Trying to set up metacon binary in /usr/local/bin"
 rm -f /usr/local/bin/metacon &> /dev/null || sudo rm -f /usr/local/bin/metacon
 ln -s ${TMPGEMDIR}/gems/${DIRNAME}/bin/metacon /usr/local/bin/metacon &> /dev/null || \
   sudo ln -s ${TMPGEMDIR}/gems/${DIRNAME}/bin/metacon /usr/local/bin/metacon
