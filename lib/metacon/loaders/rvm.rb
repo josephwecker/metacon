58c87d31071f03645cebf52d9336d98c98075ea3
Joseph Wecker <joseph.wecker@gmail.com>
2011-10-21 10:04:42 -0700

    Fix all the rvm calls, simplifying, and correctly output new bash variables etc. to be evalled

--- lib/metacon/loaders/rvm.rb
+++ lib/metacon/loaders/rvm.rb
@@ -18,8 +18,11 @@ module MetaCon
 
 
       RVMS = "source $HOME/.rvm/scripts/rvm && "
+      def self.shc(cmdstr,v); return shcmd("#{RVMS} #{cmdstr}", v) end
 
       def self.switch_ruby(ruby, state, opts={})
+        # TODO: shortcircuit all of this by trying to construct the environment path
+        # and look there first - then do all of this if it doesn't exist.
         unless check_installed(ruby,opts)
           unless install(ruby,opts)
             cfail "Failed to install ruby '#{ruby}'"
@@ -27,7 +30,7 @@ module MetaCon
           end
         end
         gemset = fix_gemset_name(state)
-        unless check_gemset_installed(gemset, opts)
+        unless check_gemset_installed(ruby, gemset, opts)
           unless create_gemset(ruby, gemset, opts)
             cfail "Failed to create a gemset '#{gemset}' for '#{ruby}'"
             return false
@@ -38,27 +41,27 @@ module MetaCon
 
 
       def self.check_installed(ruby, opts)
-        o,e,s = cmd "#{RVMS} rvm use '#{ruby}'", opts[:verbose]
+        o,e,s = shc("rvm use '#{ruby}@'", false)
         return (s==0 && o =~ /using/i)
       end
 
       def self.check_gemset_installed(ruby, gemset, opts)
-        o,e,s = cmd "#{RVMS} rvm use '#{ruby}' && rvm gemset use '#{gemset}'", opts[:verbose]
+        o,e,s = shc("rvm use '#{ruby}'@'#{gemset}'", false)
         return (s == 0 && e.strip.length == 0)
       end
 
       def self.install(ruby, opts)
-        o,e,s = cmd "#{RVMS} rvm install #{ruby}", opts[:verbose]
+        o,e,s = shc("rvm install #{ruby}", opts[:verbose])
         return (s == 0 && check_installed(ruby,opts))
       end
 
       def self.create_gemset(ruby, gemset, opts)
-        o,e,s = cmd "#{RVMS} rvm use '#{ruby}' && rvm gemset create '#{gemset}'", opts[:verbose]
+        o,e,s = shc("rvm use '#{ruby}' && rvm gemset create '#{gemset}'", opts[:verbose])
         res = (o =~ /created/i) && (s == 0) && (e.strip.length == 0)
-        return false unless res
+        return res
+        #return false unless res
         # TODO: Make sure any "permanent" prereqs are loaded (possibly
         # metacon?)
-        return true
       end
 
       def self.switch(ruby, gemset, opts)
@@ -74,10 +77,46 @@ module MetaCon
         #       * Prepend full string w/ 'bash: ' so that it gets evalled in the
         #         current context.
         #       * Enjoy!
+        identstr = `#{RVMS} rvm '#{ruby}'@'#{gemset}' do rvm tools identifier`
+        envsettings = "~/.rvm/environments/#{identstr}"
+        cmds = process_env_commands(IO.readlines(File.expand_path(envsettings)))
+        puts cmds.join("\n") if opts[:shell]
         return true
       end
 
       protected
+
+      def self.process_env_commands(cmdlines,and_exec=true)
+        res = []
+        cmdlines.each do |cl|
+          if cl =~ /^\s*export\s+PATH="([^"]+)"/
+            res << process_path_mod($1.split(':'),and_exec)
+          elsif cl =~ /^\s*unset /
+            res << ":bash #{cl.strip}"
+            ENV.delete(cl.split(/\s+/)[1..-1].join(' ')) if and_exec
+          elsif cl =~ /export /
+            if and_exec
+              val = cl.split("'")[1]
+              name = cl.split('=')[0].strip
+              ENV[name] = val
+            end
+            res << ":bash #{cl.strip}"
+          else
+            raise "Don't know what to do with the environment command: #{cl}"
+          end
+        end
+        return res
+      end
+
+      def self.process_path_mod(parts, and_exec)
+        require 'shellwords'
+        currpath = ENV['PATH'].split(':')
+        parts.delete('$PATH')
+        currpath.select!{|v| v !~ /\/\.rvm\//}
+        rewritten = (parts + currpath).join(':')
+        ENV['PATH'] = rewritten if and_exec
+        return ":bash export PATH=\"#{Shellwords.shellescape(rewritten)}\""
+      end
 
       def self.fix_ruby_version(parts)
         return parts[0].downcase=='head' ? 'ruby-head' : parts.join('-')
