abc5bb7ee7e25b590aad74105134d7488a2ba079
Joseph Wecker <jwecker@justin.tv>
2011-10-28 16:05:09 -0700

    Finds (or not) the current .metacon directory

--- ext/mvmc.c
+++ ext/mvmc.c
@@ -1,9 +1,20 @@
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
 #define streq(s1, s2) (strcmp((s1),(s2)) == 0)
 
+#define PATHSIZE 1024
+#define MCDIR "/.metacon"
+#define MCDIR_L 9
+
+char root_dir[PATHSIZE];
+
 void usage() {
-    printf("hm, ok.");
+    printf("Not really usable at all at the moment\n");
 }
 
 void error(const char* msg, ...) {
@@ -15,6 +26,37 @@ void error(const char* msg, ...) {
     va_end(ap);
 }
 
+int find_root_dir() {
+    getcwd(root_dir, PATHSIZE - MCDIR_L - 3);
+    if(root_dir == NULL) {
+        error("Couldn't open current directory");
+        exit(3);
+    }
+    int endpos = strlen(root_dir);
+    do {
+        memcpy(&root_dir[endpos], MCDIR, MCDIR_L+1);
+        if(access(root_dir, X_OK) == 0) return 1;
+    } while( (endpos = go_up(root_dir)) );
+    return 0;
+}
+
+int go_up(char *dir) {
+    int i = 0;
+    int last_pos = 0;
+    int last_pos2 = 0;
+    char ch;
+    while((ch = dir[i])) {
+        if(ch == '/' || ch == '\\') {
+            if(dir[i+1]) {
+                last_pos2 = last_pos;
+                last_pos = i; // don't count if trailing slash
+            }
+        }
+        i++;
+    }
+    return last_pos2;
+}
+
 int main(int argc, char *argv[]) {
     if(argc < 2) {
         usage();
@@ -24,7 +66,11 @@ int main(int argc, char *argv[]) {
     if(streq(argv[1], "help")) {
         usage(); return 0;
     } else if(streq(argv[1], "curr")) {
+        if(find_root_dir()) {
+            printf("----| %s |----\n", root_dir);
+        } else {
+            error("Not in a metacon project.");
+        }
     } else error("Unknown command %s. Try 'help'.", argv[1]);
 }
-
 
